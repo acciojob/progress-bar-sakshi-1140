3ae5030d5cd5b614d5903c1349d3f30e047180fd
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-06-03 21:25:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const circles = document.querySelectorAll('.circle');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+let currentStep=1;
+
+nextButton.addEventListener('click',()=>{
+    if(currentStep<circles.length)
+        currentStep++;
+    update();
+})
+
+prevButton.addEventListener('click',()=>{
+    if(currentStep>1)
+        currentStep--;
+    update()
+})
+
+function update(){
+    circles.forEach((item,index)=>{
+        if(index<currentStep)
+            item.classList.add("active")
+        else item.classList.remove('active')
+    });
+
+    prevButton.disabled= currentStep===1
+    nextButton.disabled= currentStep===circles.length
+
+}
+
+update();
